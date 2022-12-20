008b570eb5fdad5ba7cdceaeabc9b9423f750de3
Juan Sierra <juan.sierra@zabud.com.co>
2022-12-20 00:20:07 -0500

    finish keycloack and spring boot

---
+++ spring-keycloack/api-gateway/src/main/java/com/github/jsierrab3991/keycloack/apigateway/SecurityConfiguration.java
@@ -0,0 +1,2 @@
+package com.github.jsierrab3991.keycloack.apigateway;public class SecurityConfiguration {
+}
---
+++ spring-keycloack/demo-service/src/main/java/com/github/jsierrab3991/keycloack/demoservice/config/SecurityConfiguration.java
@@ -0,0 +1,4 @@
+package com.github.jsierrab3991.keycloack.demoservice.config;
+
+public class SecurityuConfiguration {
+}
