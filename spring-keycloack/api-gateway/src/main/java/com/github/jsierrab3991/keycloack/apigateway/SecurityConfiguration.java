9042d9ea477bea31779450d2ecad2d3c1df93052
Juan Sierra <juan.sierra@zabud.com.co>
2022-12-20 00:22:27 -0500

    finish keycloack and spring boot

--- spring-keycloack/api-gateway/src/main/java/com/github/jsierrab3991/keycloack/apigateway/SecurityConfiguration.java
+++ spring-keycloack/api-gateway/src/main/java/com/github/jsierrab3991/keycloack/apigateway/SecurityConfiguration.java
@@ -1,2 +1,21 @@
-package com.github.jsierrab3991.keycloack.apigateway;public class SecurityConfiguration {
+package com.github.jsierrab3991.keycloack.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+        serverHttpSecurity.csrf().disable();
+        return serverHttpSecurity.build();
+    }
 }
--- spring-keycloack/demo-service/src/main/java/com/github/jsierrab3991/keycloack/demoservice/config/SecurityConfiguration.java
+++ spring-keycloack/demo-service/src/main/java/com/github/jsierrab3991/keycloack/demoservice/config/SecurityConfiguration.java
@@ -1,4 +1,19 @@
 package com.github.jsierrab3991.keycloack.demoservice.config;
 
-public class SecurityuConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
 }
